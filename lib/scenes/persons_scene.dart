e629cfc630ce2263364d3fc8d98aa3c13fb880c1
Antonio Fasolato <antonio.fasolato@gmail.com>
2023-06-05 09:18:52 +0200

    Added persons search

--- lib/scenes/persons_scene.dart
+++ lib/scenes/persons_scene.dart
@@ -35,15 +35,20 @@ class _PersonsSceneState extends State<PersonsScene> {
   }
 
   Future<void> _getAllPersons() async {
-    var res = await PersonRepository(widget._connection).getAll();
+    List<Person> res = [];
+    if (_searchController.text.isNotEmpty) {
+      res = await PersonRepository(widget._connection)
+          .searchByName(_searchController.text);
+    } else {
+      res = await PersonRepository(widget._connection).getAll();
+    }
     setState(() {
       _persons = res;
     });
   }
 
   Widget _sidePanelBuilder() {
-    PersonState state =
-        Provider.of<PersonState>(context, listen: false);
+    PersonState state = Provider.of<PersonState>(context, listen: false);
     return state.isSelected
         ? Expanded(
             flex: 70,
@@ -92,6 +97,7 @@ class _PersonsSceneState extends State<PersonsScene> {
                     controller: _searchController,
                     decoration: const InputDecoration(
                         label: Text("Cerca"), prefixIcon: Icon(Icons.search)),
+                    onChanged: (value) => _getAllPersons(),
                   ),
                 ),
                 ListView(
@@ -101,8 +107,7 @@ class _PersonsSceneState extends State<PersonsScene> {
                             title: Text("${p.surname} ${p.name}"),
                             leading: const Icon(Icons.account_circle),
                             onTap: () {
-                              Provider.of<PersonState>(context,
-                                      listen: false)
+                              Provider.of<PersonState>(context, listen: false)
                                   .selectPerson(p);
                             },
                           ))

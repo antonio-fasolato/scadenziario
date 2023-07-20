213b312700c3f7a89c7f8d00ee1761312524fda3
Antonio Fasolato <antonio.fasolato@gmail.com>
2023-07-20 22:17:31 +0200

    Vertical and horizontal scroll

--- lib/scenes/homepage_scene.dart
+++ lib/scenes/homepage_scene.dart
@@ -11,72 +11,77 @@ class HomepageScene extends StatelessWidget {
         title: const Text("Scadenziario"),
       ),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamed("/people");
-                      },
-                      iconSize: 200,
-                      icon: const Icon(
-                        Icons.people,
-                      )),
-                  const Text(
-                    "Personale",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            Navigator.of(context).pushNamed("/people");
+                          },
+                          iconSize: 200,
+                          icon: const Icon(
+                            Icons.people,
+                          )),
+                      const Text(
+                        "Personale",
+                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamed("/calendar");
+                        },
+                        iconSize: 200,
+                        icon: const Icon(
+                          Icons.calendar_month,
+                        ),
+                      ),
+                      const Text(
+                        "Calendario",
+                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamed("/courses");
+                        },
+                        iconSize: 200,
+                        icon: const Icon(
+                          Icons.school_rounded,
+                        ),
+                      ),
+                      const Text(
+                        "Corsi",
+                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed("/calendar");
-                    },
-                    iconSize: 200,
-                    icon: const Icon(
-                      Icons.calendar_month,
-                    ),
-                  ),
-                  const Text(
-                    "Calendario",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed("/courses");
-                    },
-                    iconSize: 200,
-                    icon: const Icon(
-                      Icons.school_rounded,
-                    ),
-                  ),
-                  const Text(
-                    "Corsi",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
       bottomNavigationBar: const Footer(),
